e933dd50322323b48054474a8cc4f858ed4647c9
WillOli <william.ika@gmail.com>
2023-09-27 16:16:55 -0300

    first commit

---
+++ src/main/java/com/willdev/APIRESTSecurity/ApiRestSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.willdev.APIRESTSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willdev/APIRESTSecurity/ApiRestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willdev.APIRESTSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
